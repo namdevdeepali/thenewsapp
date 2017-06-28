c04fa5be2ec42b5ff1d69ba74e895539b07aaed5
namdevdeepali <deepali94namdev@gmail.com>
2017-06-28 23:38:49 +0530

    mwelcome hides on click , one  for loop to display the news, one fuction to use in all api's

--- custom.js
+++ custom.js
@@ -2,12 +2,19 @@
 // $("#bbcNews").click(function(){
 // 	alert("hello");
 // })
-function myFunction(){
+function myFunc(src_name){
+	$("#welcome").css("display" , "none");
 	$.ajax({
 		type:'GET',
-		url:'https://newsapi.org/v1/articles?source=techcrunch&apiKey=c6b54cdc1b504558a8e63ae5c0d13967',
+		url:'https://newsapi.org/v1/articles?source='+src_name+'&apiKey=c6b54cdc1b504558a8e63ae5c0d13967',
 		success:function(res){
 			console.log(res);
+			var newHTML = [];
+			for(var i = 0; i<res.articles.length; i++)
+			{
+			newHTML.push("<h3 style='text-align: center;'>"+res.articles[i].title+"</h3><h4 style='text-align: center;'><span style='color: blue;'>Provided by </span> :"+res.articles[i].author+" </h4><img style ='margin-left:30%' src="+res.articles[i].urlToImage+" width='400px'><p style='text-align:center;'>"+res.articles[i].description+"</p>");
+		}
+		$("#data").html(newHTML.join(""));
 		},
 		error:function(err){
 			console.log(err);
